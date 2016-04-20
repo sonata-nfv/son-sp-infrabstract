dfc131845d4593a4168ffbe655c24344f6aeb9a9
Sharon Mendel-Brin <sharon.mendel@nokia.com>
2016-04-20 19:29:56 +0300

    heat client initial version

---
+++ adaptor/src/test/java/sonata/kernel/adaptor/wrapper/OpenStackHeatClientTest.java
@@ -0,0 +1,16 @@
+package sonata.kernel.adaptor.wrapper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by smendel on 4/20/16.
+ */
+public class OpenStackHeatClientTest {
+
+    @Test
+    public void testCreateStack() throws Exception {
+
+    }
+}
