daedd059f2e5c4a5c570f231e3c70ea6ff5042dc
Sharon Mendel-Brin <sharon.mendel@nokia.com>
2016-04-21 12:12:53 +0300

    heat client split to create stack and get client

--- adaptor/src/main/java/sonata/kernel/adaptor/wrapper/OpenStackHeatClient.java
+++ adaptor/src/main/java/sonata/kernel/adaptor/wrapper/OpenStackHeatClient.java
@@ -13,15 +13,48 @@ import java.io.InputStreamReader;
 public class OpenStackHeatClient {
 
 
-    public boolean createStack(String stackName, String stack) {
+    /**
+     * Create stack
+     * @param stackName - usually service + tenant
+     * @param template - the content of the hot template thet describes the file
+     * @return
+     */
+
+    public String createStack(String stackName, String template) {
+
+        String uuid = null;
 
         try {
-              Process p = Runtime.getRuntime().exec("python heat-api.py "+ stackName + stack);
-
+            System.out.println("Creating stack: " + stackName);
+            Process p = Runtime.getRuntime().exec("python heat-api.py "+ stackName + template);
+            BufferedReader in = new BufferedReader(new InputStreamReader ((p.getInputStream())));
+            uuid = in.readLine();
+            System.out.println("UUID of new stack: " +uuid);
         }catch(Exception e){
-            return false;
+            System.out.println("Runtime error creating stack : " + stackName + " error message: "+ e.getMessage());
         }
     
-        return true;
+        return uuid;
     }
+
+
+    public String getCreateStackStatus(String stackName, String uuid){
+
+        String status = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec("python heat-api.py "+ uuid);
+            BufferedReader in = new BufferedReader(new InputStreamReader ((p.getInputStream())));
+            status = in.readLine();
+            System.out.println("The status of stack: " + stackName+ " with uuid: " + uuid +" : " + status );
+        }catch(Exception e){
+            System.out.println("Runtime error getting stack status for stack : " + stackName + " error message: "+ e.getMessage());
+        }
+
+        return status;
+
+    }
+
+
+
 }
