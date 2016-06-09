1c9f1adee2e84eb3c572b58ecc1cd7fd86eec1ef
DarioValocchi <dario.valocchi@gmail.com>
2016-06-09 16:01:55 +0100

    fixed a typo

--- vim-adaptor/test.sh
+++ vim-adaptor/test.sh
@@ -3,8 +3,6 @@ set -e
 echo "Setting environment"
 /setenv.sh
 
-echo "Creating user and database"
-
 echo "Doing maven test"
 cd /adaptor
 mvn -Dcheckstyle.config.location=google_checks.xml checkstyle:checkstyle findbugs:findbugs cobertura:cobertura
