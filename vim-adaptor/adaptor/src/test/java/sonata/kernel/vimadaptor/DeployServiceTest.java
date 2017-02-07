19e3a5b144fd6e5fc22fcb806f809ef58b48a421
DarioValocchi <dario.valocchi@gmail.com>
2017-02-07 18:55:30 +0000

    Deactivated infrastructure dependant tests.

--- vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
+++ vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
@@ -413,7 +413,7 @@ public class DeployServiceTest implements MessageReceiver {
    *
    * @throws Exception
    */
-  @Test
+  @Ignore
   public void testDeployServiceOpenStackV1() throws Exception {
 
     BlockingQueue<ServicePlatformMessage> muxQueue =
