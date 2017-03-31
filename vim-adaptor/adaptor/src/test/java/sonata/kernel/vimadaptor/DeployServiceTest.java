0556aeba57b146ed354a9f81979cf73f376bbcb6
DarioValocchi <dario.valocchi@gmail.com>
2017-03-31 16:18:37 +0100

    De-activated infrastructure dependant tests

--- vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
+++ vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
@@ -1014,7 +1014,7 @@ public class DeployServiceTest implements MessageReceiver {
    * 
    * @throws Exception
    */
-  @Test
+  @Ignore
   public void testDeployServiceIncremental() throws Exception {
     BlockingQueue<ServicePlatformMessage> muxQueue =
         new LinkedBlockingQueue<ServicePlatformMessage>();
@@ -1312,7 +1312,7 @@ public class DeployServiceTest implements MessageReceiver {
    * 
    * @throws Exception
    */
-  @Test
+  @Ignore
   public void testDeployServiceIncrementalMultiPoP() throws Exception {
     BlockingQueue<ServicePlatformMessage> muxQueue =
         new LinkedBlockingQueue<ServicePlatformMessage>();
