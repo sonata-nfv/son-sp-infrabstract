fa3940e2500cf2d7784c5cb46e22533141b1334f
Stavros Kolometsos <stkolome@iit.demokritos.gr>
2017-05-24 20:12:09 +0300

    Added POST & DELETE

--- wim-adaptor/adaptor/vtn-api/utils.py
+++ wim-adaptor/adaptor/vtn-api/utils.py
@@ -4,7 +4,7 @@ import json
 import logging 
 
 
-def set_condition(cond_name, vbr, source, dest):
+def set_condition(cond_name, source, dest):
     s_url = 'operations/vtn-flow-condition:set-flow-condition'
     username, password, host, url, headers = get_info()
     data = {'input': {'name': cond_name, 'vtn-flow-match': [
@@ -17,11 +17,20 @@ def set_condition(cond_name, vbr, source, dest):
     '''
     r = requests.post(url + s_url, headers=headers,
                       auth=(username, password), json=data)
-    print r
+    logging.debug("Got this as response: " +str(r) )
     if not r.status_code == 200:
-        print 'FLOW COND ERROR ' + str(r.status_code)
-        exit(1)
+        logging.error('FLOW COND ERROR ' + str(r.status_code))
+        #exit(1)
 
+
+def delete_condition(cond_name):
+    s_url = 'operations/vtn-flow-condition:remove-flow-condition'
+    username, password, host, url, headers = get_info()
+    data = {'input': {'name': cond_name}}
+    r = requests.post(url+s_url, headers=headers, auth=(username, password), json=data)
+    logging.debug("Got response:" +str(r))
+    if not r.status_code == 200:
+    	logging.error("Condition removal ERROR " + str(r.status_code))
 
 def set_redirect(cond_name, vbr, port_id_in, port_id_out):
     s_url = 'operations/vtn-flow-filter:set-flow-filter'
@@ -30,14 +39,14 @@ def set_redirect(cond_name, vbr, port_id_in, port_id_out):
         {"index": "1", "condition": cond_name, "vtn-redirect-filter": {"redirect-destination": {"bridge-name": vbr, "interface-name": port_id_out}, "output": "true"}}]}}
     '''
      this: curl --user "username":"pass" -H "Content-type: application/json" -X POST http://localhost:8181/restconf/operations/vtn-flow-filter:set-flow-filter
-      -d '{"input":{"output":"false","tenant-name":"vtn1", "bridge-name":"interface-name":"if5","vtn-flow-filter":[{"condition":"cond_1","index":"1","vtn-redirect-filter":
+      -d '{"input":{"output":"false","tenant-name":"vtn1", "bridge-name":"vbr", interface-name":"if5", "vtn-flow-filter":[{"condition":"cond_1","index":"1","vtn-redirect-filter":
       {"redirect-destination":{"bridge-name":"vbr1","interface-name":"if3"},"output":"true"}}]}}'
 	'''
     r = requests.post(url + s_url, headers=headers,
                       auth=(username, password), json=data)
     if not r.status_code == 200:
-        print 'FLOW FILTER ERROR ' + str(r.status_code)
-        exit(1)
+        logging.error('FLOW FILTER ERROR ' + str(r.status_code))
+        #exit(1)
 
 
 def get_vtn_name():
