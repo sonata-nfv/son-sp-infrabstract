32f2ce14f2eec5d07f8ff2687fed361bcb2b4aa0
Stavros Kolometsos <stkolome@iit.demokritos.gr>
2017-05-26 17:57:58 +0300

    progress

--- wim-adaptor/adaptor/vtn-api/flowchart.py
+++ wim-adaptor/adaptor/vtn-api/flowchart.py
@@ -3,20 +3,22 @@ from flask_restful import Resource
 from flask import jsonify, request
 import utils
 
+flows = []
+
 class FlowChart(Resource):
 	
-	flows  = []
+	
 
 	def get(self):
-		logging.debug("call for flow chart. Returning: " + str(self.flows))
-		return str(self.flows)
+		logging.debug("call for flow chart. Returning: " + str(flows))
+		return str(flows)
 
 	def post(self):
 		data = request.json
 		cond_name = data["instance_id"]
 		in_seg = data["in_seg"]
 		out_seg = data["out_seg"]
-		ports = data["ports"]
+		order_pop = data["order_pop"]
 		logging.debug("Calling set_condition method")
 		utils.set_condition(cond_name,in_seg, out_seg)
 		logging.info("Conditon set completed")
@@ -27,13 +29,22 @@ class FlowChart(Resource):
 		# utils.set_redirect(cond_name, vbr, port_id_in, port_id_out)
 
 		flow = {'data': data}
-		self.flows.append(flow)
+		flows.append(flow)
 		logging.debug("call to POST new flow chart")
 		return data
 
-	def delete(self):
-		data = request.jso
-		cond_name = data["instance_id"]
-		logging.debug("Call to delete condition: "+cond_name)
-		utils.delete_condition(cond_name)
+class Flows(Resource):
+
+	def get(self, res_name):
+		for flow in flows:
+			if flow['data']['instance_id'] == res_name:
+				return flow 
+
+	def delete(self, res_name):
+		logging.debug("Call to delete condition: " +res_name)
+		utils.delete_condition(res_name)
+		for flow in flows:
+			if flow['data']['instance_id'] == res_name:
+				flows.remove(flow)
 		return "Success"
+
