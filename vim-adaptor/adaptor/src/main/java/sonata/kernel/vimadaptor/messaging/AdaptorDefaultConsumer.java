3ff7bd86534cec99ae10241a2b761713ef6b6e28
DarioValocchi <dario.valocchi@gmail.com>
2017-07-17 10:53:42 +0100

    Moved ack in finally block

--- vim-adaptor/adaptor/src/main/java/sonata/kernel/vimadaptor/messaging/AdaptorDefaultConsumer.java
+++ vim-adaptor/adaptor/src/main/java/sonata/kernel/vimadaptor/messaging/AdaptorDefaultConsumer.java
@@ -40,8 +40,8 @@ import java.io.IOException;
 
 public class AdaptorDefaultConsumer extends DefaultConsumer {
 
-  
-  
+
+
   private static final org.slf4j.Logger Logger =
       LoggerFactory.getLogger(AdaptorDefaultConsumer.class);
 
@@ -61,21 +61,24 @@ public class AdaptorDefaultConsumer extends DefaultConsumer {
   @Override
   public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
       byte[] body) throws IOException {
-    String message = new String(body, "UTF-8");
-    this.getChannel().basicAck(envelope.getDeliveryTag(), false);
-    // Logger.info("Received message:" + message + " on " + envelope.getRoutingKey());
-    Logger.info("Received message on " + envelope.getRoutingKey());
-    if (properties != null && properties.getAppId() != null
-        && properties.getAppId().equals("sonata.kernel.WimAdapter")
-        && envelope.getRoutingKey().equals("infrastructure.service.deploy")) {
-      Logger.info("Ignoring WIM adaptor response after service deployment");
-    } else if (properties != null && properties.getAppId() != null
-        && !properties.getAppId().equals(AdaptorCore.APP_ID)) {
-      Logger.debug("message SID-" + properties.getCorrelationId());
-      this.msgBusConsumer.processMessage(message, properties.getContentType(),
-          envelope.getRoutingKey(), properties.getCorrelationId(), properties.getReplyTo());
-    } else {
-      Logger.info("Message ignored: " + properties);
+    try {
+      String message = new String(body, "UTF-8");
+      Logger.info("Received message on " + envelope.getRoutingKey());
+      // Logger.info("Received message:" + message + " on " + envelope.getRoutingKey());
+      if (properties != null && properties.getAppId() != null
+          && properties.getAppId().equals("sonata.kernel.WimAdapter")
+          && envelope.getRoutingKey().equals("infrastructure.service.deploy")) {
+        Logger.info("Ignoring WIM adaptor response after service deployment");
+      } else if (properties != null && properties.getAppId() != null
+          && !properties.getAppId().equals(AdaptorCore.APP_ID)) {
+        Logger.debug("message SID-" + properties.getCorrelationId());
+        this.msgBusConsumer.processMessage(message, properties.getContentType(),
+            envelope.getRoutingKey(), properties.getCorrelationId(), properties.getReplyTo());
+      } else {
+        Logger.info("Message ignored: " + properties);
+      }
+    } finally {
+      this.getChannel().basicAck(envelope.getDeliveryTag(), false);
     }
   }
 
