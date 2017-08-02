dcaf08ca57715559b50a6fc2a933cfb772f98098
DarioValocchi <dario.valocchi@gmail.com>
2017-08-02 17:29:24 +0200

    Vim Adaptor infrastructure-dependant test activated

--- vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
+++ vim-adaptor/adaptor/src/test/java/sonata/kernel/vimadaptor/DeployServiceTest.java
@@ -344,7 +344,7 @@ public class DeployServiceTest implements MessageReceiver {
    *
    * @throws Exception
    */
-  @Ignore
+  @Test
   public void testDeployServiceV2() throws Exception {
     BlockingQueue<ServicePlatformMessage> muxQueue =
         new LinkedBlockingQueue<ServicePlatformMessage>();
