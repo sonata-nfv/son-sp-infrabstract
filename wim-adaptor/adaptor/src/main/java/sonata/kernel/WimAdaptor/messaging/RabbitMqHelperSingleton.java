fa395978ff63a92a9c4cb96f5d475bbc02e9ac68
DarioValocchi <dario.valocchi@gmail.com>
2017-08-21 13:27:58 +0200

    Wim-Adaptor: Fixed a c/p bug on the RabbitMq singleton

--- wim-adaptor/adaptor/src/main/java/sonata/kernel/WimAdaptor/messaging/RabbitMqHelperSingleton.java
+++ wim-adaptor/adaptor/src/main/java/sonata/kernel/WimAdaptor/messaging/RabbitMqHelperSingleton.java
@@ -74,7 +74,7 @@ public class RabbitMqHelperSingleton {
       channel = connection.createChannel();
       exchangeName = brokerConfig.getProperty("exchange");
       channel.exchangeDeclare(exchangeName, "topic");
-      queueName = exchangeName + "." + "InfraAbstract";
+      queueName = exchangeName + "." + "WimAdaptor";
       channel.queueDeclare(queueName, true, false, false, null);
       Logger.info("Binding queue to topics...");
 
