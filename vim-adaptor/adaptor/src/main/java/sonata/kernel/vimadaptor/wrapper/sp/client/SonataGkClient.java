b98968fba61774379c0fc0e46d91efd1c27496fa
DarioValocchi <dario.valocchi@gmail.com>
2017-12-15 11:11:56 +0100

    removed un-used method

--- vim-adaptor/adaptor/src/main/java/sonata/kernel/vimadaptor/wrapper/sp/client/SonataGkClient.java
+++ vim-adaptor/adaptor/src/main/java/sonata/kernel/vimadaptor/wrapper/sp/client/SonataGkClient.java
@@ -1,31 +1,15 @@
 package sonata.kernel.vimadaptor.wrapper.sp.client;
 
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.TrustStrategy;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -467,60 +451,6 @@ public class SonataGkClient {
     VnfRecord functionRecord = mapper.readValue(stringResponse, VnfRecord.class);
 
     return functionRecord;
-  }
-
-
-  private HttpClient createHttpClientAcceptsUntrustedCerts() {
-    HttpClientBuilder b = HttpClientBuilder.create();
-
-    // setup a Trust Strategy that allows all certificates.
-    //
-    SSLContext sslContext;
-    HttpClient client = null;
-    try {
-      sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-        public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
-          return true;
-        }
-      }).build();
-      b.setSSLContext(sslContext);
-
-      // don't check Hostnames, either.
-      // -- use SSLConnectionSocketFactory.getDefaultHostnameVerifier(), if you don't want to weaken
-      HostnameVerifier hostnameVerifier = SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
-
-      // here's the special part:
-      // -- need to create an SSL Socket Factory, to use our weakened "trust strategy";
-      // -- and create a Registry, to register it.
-      //
-      SSLConnectionSocketFactory sslSocketFactory =
-          new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
-      Registry<ConnectionSocketFactory> socketFactoryRegistry =
-          RegistryBuilder.<ConnectionSocketFactory>create()
-              .register("http", PlainConnectionSocketFactory.getSocketFactory())
-              .register("https", sslSocketFactory).build();
-
-      // now, we create connection-manager using our Registry.
-      // -- allows multi-threaded use
-      PoolingHttpClientConnectionManager connMgr =
-          new PoolingHttpClientConnectionManager(socketFactoryRegistry);
-      b.setConnectionManager(connMgr);
-
-      // finally, build the HttpClient;
-      // -- done!
-      client = b.build();
-
-    } catch (KeyManagementException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (NoSuchAlgorithmException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (KeyStoreException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return client;
   }
 
 }
